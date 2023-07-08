05fd9094542577d74502df5af396e9276baafba2
Takuya Asai <loveyasa11111@gmail.com>
2023-07-08 16:41:21 +0900

    section2

---
+++ section2/components/GoalItem.js
@@ -0,0 +1,33 @@
+import { StyleSheet, View, Text, Pressable } from "react-native";
+
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      {/* render後に即座に値が評価されてしまうのでbindを使ってボタンが押された時に評価されるようにする */}
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+});
